542adf34252a6dcf08d2c84882cc64fdb9466c67
qisen <821433202@qq.com>
2017-12-04 17:14:54 +0800

    Revert "Revert "提交修改可以保存图片""
    This reverts commit 2b9a820da3900ee7353879a5a12b7d1ac2b60f3f.

--- scrapyspider/spiders/douban_spider.py
+++ scrapyspider/spiders/douban_spider.py
@@ -10,21 +10,31 @@ from scrapyspider.items import DoubanMovieItem
 class DoubanMovieTop250Spider(Spider):
     name = 'jiandan'
     allowed_domains = ["jandan.net"]
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0',
-    }
+    # headers ={
+    #     'User-Agent': 'Mozilla / 5.0(WindowsNT6.1;WOW64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 60.0.3112.113Safari / 537.36',
+    # }
+    #http://blog.csdn.net/djd1234567/article/details/50319329   防止屏蔽
+    #http://blog.csdn.net/qq_31518899/article/details/76576537  数据存储
     start_urls = ["http://jandan.net/ooxx"]
     maxNum = 10
     current = 0
 
     def parse(self, response):
         item = DoubanMovieItem()
-        item['image_urls'] = response.xpath('//img//@src').extract()  # 提取图片链接
-        # print 'image_urls',item['image_urls']
-        yield item
+        strpp=response.xpath('//img//@src').extract()
+        print("response",response.body)
+        # return
+        # strpplist=[]
+        # print("strpp",strpp)
+        # for i in range(len(strpp)):
+        #     strpplist.insert(i,strpp[i] if 'http:' in strpp[i] else ('http:' + strpp[i]))
+        #     item['image_urls'] = strpplist# 提取图片链接
+        #
+        # # print('image_urls',item['image_urls'])
+        # yield item
         self.current = self.current + 1
         if self.maxNum > self.current:
             new_url = response.xpath('//a[@class="previous-comment-page"]//@href').extract_first()  # 翻页
             # print 'new_url',new_url
             if new_url:
-                yield Request(new_url, callback=self.parse)
+               yield Request(new_url if 'http:' in new_url else ('http:' + new_url), callback=self.parse)
